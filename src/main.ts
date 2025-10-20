61e4a310e64d22558c314f1f05426a0c41d02b13
JQuinzell <>
2025-10-20 15:23:59 -0500

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { BrowserWindow, app } from 'electron'
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
